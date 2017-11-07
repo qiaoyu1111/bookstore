c2c4f94b0ef236c552b73e51dcbcc285cbd6593f
qiaoyu <qiaoyu1111358@gmail.com>
2017-11-07 10:08:56 +0800

    implement devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   resources :books
   root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
